7afb776c59ef281fb9445cea03e8779312af1f93
claudio-2019 <claudiogh33@gmail.com>
2022-03-02 23:11:25 -0600

    src commit

---
+++ src/main/java/com/example/ticonetgamesserver/TicoNetGamesServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticonetgamesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicoNetGamesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicoNetGamesServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ticonetgamesserver/TicoNetGamesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticonetgamesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicoNetGamesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
